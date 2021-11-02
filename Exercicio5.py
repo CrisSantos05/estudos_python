365d0f4762cfaca707759f1ded70a941df7355c1
CrisSantos05 <cristianosantos0584@gmail.com>
2021-11-02 18:50:12 -0300

    atualizaçãoEX5

--- Exercicio5.py
+++ Exercicio5.py
@@ -5,8 +5,8 @@ Pos = 'sim'
 
 while Pos != 'não':
     nome = input('Seu nome aqui: ')
-    idade = input('Sua idade aqui: ')
-    peso = input('Seu peso aqui: ')
+    idade = int(input('Sua idade aqui: '))
+    peso = float(input('Seu peso aqui: '))
     cont = input('Quer continuar ?: ')
     Pos = cont
     print('____________________________________________________________________________________________')
