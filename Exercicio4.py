4e2a21c3c7b2627e0811a78b31b14d08f8a236b8
CrisSantos05 <cristianosantos0584@gmail.com>
2021-11-02 18:40:08 -0300

    exerciciosPython2

---
+++ Exercicio4.py
@@ -0,0 +1,13 @@
+numero = input('Digite aqui: ')
+#nome = input('Digite seu nome aqui: ')
+
+
+print('o valor é :',numero.isalnum())
+print('o valor é :',numero.islower())
+print('o valor é :',numero.isdecimal())
+print('o valor é :',numero.isnumeric())
+print('o valor é :',numero.isupper())
+print('o valor é :',numero.isalpha())
+print('o valor é :',numero.isspace())
+print('o valor é :',numero.isascii())
+
---
+++ Exercicio5.py
@@ -0,0 +1,15 @@
+Pos = 'sim'
+
+#Programa que faz loop de cadastro enquanto a pergunta de continuação for sim!
+
+
+while Pos != 'não':
+    nome = input('Seu nome aqui: ')
+    idade = input('Sua idade aqui: ')
+    peso = input('Seu peso aqui: ')
+    cont = input('Quer continuar ?: ')
+    Pos = cont
+    print('____________________________________________________________________________________________')
+
+print('Cadastro Encerrado')
+
