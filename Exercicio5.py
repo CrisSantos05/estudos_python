b645a55e2d401a57d6c34ca5b09185b1e804e462
CrisSantos05 <cristianosantos0584@gmail.com>
2021-11-03 11:37:45 -0300

    atualizaçãoEX.5

--- Exercicio5.py
+++ Exercicio5.py
@@ -1,9 +1,13 @@
+import random
+
 Pos = 'sim'
 
 #Programa que faz loop de cadastro enquanto a pergunta de continuação for sim!
 
 
 while Pos != 'não':
+
+    print('Número de cadastro gerado: ',random.randrange(1,101))
     nome = input('Seu nome aqui: ')
     idade = int(input('Sua idade aqui: '))
     peso = float(input('Seu peso aqui: '))
